701c70a0ccb5eb04a9885b7b0632d5d2ea820e5f
Zaki <ikaz.enissay@gmail.com>
2025-10-16 14:08:58 +0200

    Yassine: commented the ones I think unimportant

--- project_script/list.py
+++ project_script/list.py
@@ -4,92 +4,92 @@ cal_var = [
     '_RFHLTH', #Adults with good or better health
     '_HCVU651', #Respondents aged 18-64 who have any form of health care coverage
     '_RFHYPE5', #Adults who have been told they have high blood pressure by a doctor, nurse, or other health professional
-    '_CHOLCHK', #Cholesterol check within past five years
+    #RFCHOL better'_CHOLCHK', #Cholesterol check within past five years
     '_RFCHOL', #adults who have had their cholesterol checked and have been told by a doctor, nurse, or other health professional that it was high 59k nan
     '_MICHD', #Respondents that have ever reported having coronary heart disease (CHD) or myocardial infarction (MI) 3k nan
     '_LTASTH1', #Adults who have ever been told they have asthma
     '_CASTHM1', #Adults who have been told they currently have asthma
-    '_ASTHMS1', #Computed asthma status
+    #it takes two data '_ASTHMS1', #Computed asthma status
     '_DRDXAR1', #Respondents who have had a doctor diagnose them as having some form of arthritis
-    '_PRACE1', #Preferred race category
-    '_MRACE1', #Calculated multiracial race categorization
-    '_HISPANC', #Hispanic, Latino/a, or Spanish origin calculated variable
+    #unimportant'_PRACE1', #Preferred race category
+    #unimportant'_MRACE1', #Calculated multiracial race categorization
+    #unimportant'_HISPANC', #Hispanic, Latino/a, or Spanish origin calculated variable
     '_RACE', #Race/ethnicity categories
-    '_RACEG21', #White non-Hispanic race group
-    '_RACEGR3', #Five-level race/ethnicity category
-    '_RACE_G1', #Race groups used for internet prevalence tables 7k nan
-    '_AGEG5YR', #Fourteen-level age category
-    '_AGE65YR', #Two-level age category
-    '_AGE80', #Imputed Age value collapsed above 80
+    #unimportant'_RACEG21', #White non-Hispanic race group
+    #unimportant'_RACEGR3', #Five-level race/ethnicity category
+    #unimportant'_RACE_G1', #Race groups used for internet prevalence tables 7k nan
+    #unclean'_AGEG5YR', #Fourteen-level age category
+    #unimportant'_AGE65YR', #Two-level age category
+    #unimportant'_AGE80', #Imputed Age value collapsed above 80
     '_AGE_G', #Six-level imputed age category
-    '_HTIN4', #Reported height in inches #17k nan
-    '_HTM4', #Reported height in meters 15k nan
-    '_WTKG3', #Reported weight in kilograms
-    '_BMI5', #Body Mass Index (BMI) 36k nan
+    #unimportant'_HTIN4', #Reported height in inches #17k nan
+    #unimportant'_HTM4', #Reported height in meters 15k nan
+    #unimportant'_WTKG3', #Reported weight in kilograms
+    #BMI5CAT'_BMI5', #Body Mass Index (BMI) 36k nan
     '_BMI5CAT', #Four-categories of Body Mass Index (BMI) 36k nan
-    '_RFBMI5', #Adults who have a body mass index greater than 25.00 (Overweight or Obese) 36k nan
+    #BMI5CAT'_RFBMI5', #Adults who have a body mass index greater than 25.00 (Overweight or Obese) 36k nan
     '_CHLDCNT', #Number of children in household
     '_EDUCAG', #Level of education completed
-    '_INCOMG', #Income categories
+    #unimportant'_INCOMG', #Income categories
     '_SMOKER3', #Four-level smoker status: Everyday smoker, Someday smoker, Former smoker, Non-smoker
-    '_RFSMOK3', #Adults who are current smokers
-    '_DRNKANY5', #Adults who reported having had at least one drink of alcohol in the past 30 days.
-    '_DROCDY3_', #Drink-occasions-per-day
-    '_RFBING5', #Binge drinkers (males having five or more drinks on one occasion, females having four or more drinks on one occasion)
+    #unimportant'_RFSMOK3', #Adults who are current smokers
+    #unimportant'_DRNKANY5', #Adults who reported having had at least one drink of alcohol in the past 30 days.
+    #unimportant'_DROCDY3_', #Drink-occasions-per-day
+    #unimportant'_RFBING5', #Binge drinkers (males having five or more drinks on one occasion, females having four or more drinks on one occasion)
     '_DRNKWEK', #Calculated total number of alcoholic beverages consumed per week
-    '_RFDRHV5', #Heavy drinkers (adult men having more than 14 drinks per week and adult women having more than 7 drinks per
-    '_FTJUDA1_', #Fruit juice intake in times per day 38k nan
-    '_FRUTDA1_', #Fruit intake in times per day 36k nan
-    '_BEANDAY_', #Bean intake in times per day 39k nan
-    '_GRENDAY_', #Dark green vegetable intake in times per day 38k nan
-    '_ORNGDAY_', #Orange-colored vegetable intake in times per day 39k nan
-    '_VEGEDA1_', #Other vegetable intake in times per day 41k nan
-    '_MISFRTN', #The number of missing fruit responses
-    '_MISVEGN', #The number of missing vegetable responses
-    '_FRTRESP', #Missing any fruit responses
-    '_VEGRESP', #Missing any vegetable responses
-    '_FRUTSUM', #Total fruits consumed per day 43k nan
-    '_VEGESUM', #Total vegetables consumed per day 51k nan
-    '_FRTLT1', #Consume Fruit 1 or more times per day
-    '_VEGLT1', #Consume Vegetables 1 or more times per day
-    '_FRT16', #Reported consuming Fruit >16 per day
-    '_VEG23', #Reported consuming Vegetables >23 per day
+    #unimportant'_RFDRHV5', #Heavy drinkers (adult men having more than 14 drinks per week and adult women having more than 7 drinks per
+    # '_FTJUDA1_', #Fruit juice intake in times per day 38k nan
+    # '_FRUTDA1_', #Fruit intake in times per day 36k nan
+    # '_BEANDAY_', #Bean intake in times per day 39k nan
+    # '_GRENDAY_', #Dark green vegetable intake in times per day 38k nan
+    # '_ORNGDAY_', #Orange-colored vegetable intake in times per day 39k nan
+    # '_VEGEDA1_', #Other vegetable intake in times per day 41k nan
+    # #unimportant'_MISFRTN', #The number of missing fruit responses
+    # #unimportant'_MISVEGN', #The number of missing vegetable responses
+    # #unimportant'_FRTRESP', #Missing any fruit responses
+    # #unimportant'_VEGRESP', #Missing any vegetable responses
+    # '_FRUTSUM', #Total fruits consumed per day 43k nan
+    # '_VEGESUM', #Total vegetables consumed per day 51k nan
+    # '_FRTLT1', #Consume Fruit 1 or more times per day
+    # '_VEGLT1', #Consume Vegetables 1 or more times per day
+    # '_FRT16', #Reported consuming Fruit >16 per day
+    # '_VEG23', #Reported consuming Vegetables >23 per day
     '_FRUITEX', #Fruit Exclusion from analyses
     '_VEGETEX', #Vegetable Exclusion from analyses
-    '_TOTINDA', #Adults who reported doing physical activity or exercise during the past 30 days other than their regular job
-    '_METVL11_', #Activity MET Value for First Activity 146k nan
-    '_METVL21_', #Activity MET Value for Second Activity 151k nan
-    '_MAXVO2_', #Estimated Age-Gender Specific Maximum Oxygen Consumption
-    '_FC60_', #Estimated Functional Capacity
-    '_ACTIN11_', #Estimated Activity Intensity for First Activity 150k nan
-    '_ACTIN21_', #Estimated Activity Intensity for Second Activity 154k nan
-    '_PADUR1_', #Estimated Duration in Minutes for First Activity 154k nan
-    '_PADUR2_', #Minutes of Second Activity 249k nan
-    '_PAFREQ1_', #Physical Activity Frequency per Week for First Activity 150k nan
-    '_PAFREQ2_', #Physical Activity Frequency per Week for Second Activity 246k nan
-    '_MINAC11_', #Minutes of Physical Activity per week for First Activity 155k nan
-    '_MINAC21_', #Minutes of Physical Activity per week for Second Activity 157k nan
-    '_STRFREQ_', #Strength Activity Frequency per Week 44k nan
-    '_PAMISS1_', #Missing Physical Activity Data
-    '_PAMIN11_', #Minutes of Physical Activity per week for First Activity 158k nan
-    '_PAMIN21_', #Minutes of Physical Activity per week for Second Activity 160k nan
-    '_PA1MIN_', #Minutes of total Physical Activity per week 152k nan
-    '_PAVIG11_', #Minutes of Vigorous Physical Activity per week for First Activity 153k nan
-    '_PAVIG21_', #Minutes of Vigorous Physical Activity per week for Second Activity 158k nan
-    '_PA1VIGM_', #Minutes of total Vigorous Physical Activity per week 150k nan
-    '_PACAT1_', #Physical Activity Categories
-    '_PAINDX1_', #Physical Activity Index
-    '_PA150R2_', #Adults that participated in 150 minutes (or vigorous equivalent minutes) of physical activity per week.
-    '_PA300R2_', #Adults that participated in 300 minutes (or vigorous equivalent minutes) of physical activity per week.
-    '_PA30021_', #Adults that participated in 300 minutes (or vigorous equivalent minutes) of physical activity per week (2-levels).
-    '_PASTRNG_', #Muscle Strengthening Recommendation
+    # '_TOTINDA', #Adults who reported doing physical activity or exercise during the past 30 days other than their regular job
+    # '_METVL11_', #Activity MET Value for First Activity 146k nan
+    # '_METVL21_', #Activity MET Value for Second Activity 151k nan
+    # '_MAXVO2_', #Estimated Age-Gender Specific Maximum Oxygen Consumption
+    # '_FC60_', #Estimated Functional Capacity
+    # '_ACTIN11_', #Estimated Activity Intensity for First Activity 150k nan
+    # '_ACTIN21_', #Estimated Activity Intensity for Second Activity 154k nan
+    # '_PADUR1_', #Estimated Duration in Minutes for First Activity 154k nan
+    # '_PADUR2_', #Minutes of Second Activity 249k nan
+    # '_PAFREQ1_', #Physical Activity Frequency per Week for First Activity 150k nan
+    # '_PAFREQ2_', #Physical Activity Frequency per Week for Second Activity 246k nan
+    # '_MINAC11_', #Minutes of Physical Activity per week for First Activity 155k nan
+    # '_MINAC21_', #Minutes of Physical Activity per week for Second Activity 157k nan
+    # '_STRFREQ_', #Strength Activity Frequency per Week 44k nan
+    # '_PAMISS1_', #Missing Physical Activity Data
+    # '_PAMIN11_', #Minutes of Physical Activity per week for First Activity 158k nan
+    # '_PAMIN21_', #Minutes of Physical Activity per week for Second Activity 160k nan
+    # '_PA1MIN_', #Minutes of total Physical Activity per week 152k nan
+    # '_PAVIG11_', #Minutes of Vigorous Physical Activity per week for First Activity 153k nan
+    # '_PAVIG21_', #Minutes of Vigorous Physical Activity per week for Second Activity 158k nan
+    # '_PA1VIGM_', #Minutes of total Vigorous Physical Activity per week 150k nan
+    # '_PACAT1_', #Physical Activity Categories
+    # '_PAINDX1_', #Physical Activity Index
+    # '_PA150R2_', #Adults that participated in 150 minutes (or vigorous equivalent minutes) of physical activity per week.
+    # '_PA300R2_', #Adults that participated in 300 minutes (or vigorous equivalent minutes) of physical activity per week.
+    # '_PA30021_', #Adults that participated in 300 minutes (or vigorous equivalent minutes) of physical activity per week (2-levels).
+    # '_PASTRNG_', #Muscle Strengthening Recommendation
     '_PAREC1_', #Aerobic and Strengthening Guideline
-    '_PASTAE1_', #Aerobic and Strengthening (2-level)
+    #'_PASTAE1_', #Aerobic and Strengthening (2-level)
     '_LMTACT1_', #Limited usual activities 3k nan
     '_LMTWRK1_', #Limited work activities 3k nan
     '_LMTSCL1_', #Limited social activities 3k nan
-    '_RFSEAT2_', #Always or Nearly Always Wear Seat Belts Calculated Variable
-    '_RFSEAT3_', #Always Wear Seat Belts Calculated Variable
+    #unimportant'_RFSEAT2_', #Always or Nearly Always Wear Seat Belts Calculated Variable
+    #unimportant'_RFSEAT3_', #Always Wear Seat Belts Calculated Variable
     '_FLSHOT6_', #Adults aged 65+ who have had a flu shot within the past year, 283k nan
     '_PNEUMO2_', #Adults aged 65+ who have ever had a pneumonia vaccination 283k nan
     '_AIDTST3_' #Adults who have ever been tested for HIV 43k nan
